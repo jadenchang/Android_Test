2e40865feabca1c04db30ff61b2b2636b275c054
jaden.chang <jaden.chang@effision.com>
2021-11-16 07:26:07 +1100

    made a change

--- app/src/main/java/com/example/jadenchang/android_test/MainActivity.java
+++ app/src/main/java/com/example/jadenchang/android_test/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void test(){
+        //do it.
+    }
 }
+
+
